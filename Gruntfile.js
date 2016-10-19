02409db9d00150829afd907a0f5ffd5068451821
AlfonsoMac <alfonso.chaparro.m@gmail.com>
2016-10-19 12:08:21 +0200

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		connect: {
+			dev: {
+				port: 8000,
+				base: 'web'
+			}
+		},
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'web/styles/scss',
+					cssDir: 'web/styles/css'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['compass']
+			}
+		},
+		concurrent: {
+			options: {
+				logConcurrentOutput: true
+			},
+			dev: {
+				tasks: ['connect:dev', 'watch']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-connect');
+	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['concurrent:dev']);
+
+};
